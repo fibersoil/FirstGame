376d0175b2aa8d84879adaf7000ba6495c496761
fibersoil <858657154@qq.com>
2024-08-21 13:32:56 +0800

    Game Mode and Player Blueprints

---
+++ Source/FirstGame/FirstGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FirstGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FirstGame, "FirstGame" );
